6966fa24e8acae871d09c9eb7d43a803e52b5ee5
Edwin(t5610) <esaavedrac@u.northwestern.edu>
2022-06-15 16:02:52 -0500

    refactored richardsFoam compiled

---
+++ solvers/RichardsFoamE/RichardsFoamE.C
@@ -0,0 +1,111 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    richardsFoamE
+
+Description
+    Refactor RichardsFoam3 to adapt to a reactive transport solver
+
+    Solves for saturation and head:
+    - Water saturation (theta)
+    - Head (h)
+
+    Eqs:
+    
+    
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "fvOptions.H"
+#include "simpleControl.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    
+    const volScalarField& z (mesh.C().component(vector::Z));
+    simpleControl simple(mesh);
+
+    #include "readParameters.H"
+    #include "createFields.H"
+    #include "createFvOptions.H"
+   
+    Foam::Info << "\nCalculating...\n" << endl;
+    
+    while (simple.loop(runTime))
+    {
+        Foam::Info<< "Time = " << runTime.timeName() << nl << endl;
+        
+        // Solve saturation from Richards Equation    
+        while (simple.correctNonOrthogonal())
+        {
+            // Calculate effective saturation from van Genutchen eq.
+            sat_e = Foam::pow(1 + Foam::pow(soil.alpha * h, soil.n) , - soil.m);
+            
+            // Calculate saturation
+            sat = sat_e * (soil.sat_s - soil.sat_r) + soil.sat_r;
+
+            // Calculate relative permebility
+            perm = Foam::sqrt(sat_e) * Foam::sqr(1.0 - Foam::pow(1-Foam::pow(sat_e, 1.0/soil.m), soil.m));
+
+            // Calculate calpillary capacity
+            capillary = fvc::ddt(sat)/fvc::ddt(h);
+
+            // Calculate hydraulic conductivity 
+            hydrConduct = soil.K_s * perm;
+
+            // Solve Richard's equation    
+            fvScalarMatrix richardsEquation
+            (
+                fvm::ddt(capillary,h)
+                - fvm::laplacian(hydrConduct, h)
+                ==
+                fvc::laplacian(hydrConduct, z)
+            );
+            richardsEquation.relax();
+            fvOptions.constrain(richardsEquation);
+            richardsEquation.solve();
+            fvOptions.correct(h);
+        }
+        
+        // Calculate Darcy flow velocity
+        U = soil.K_s * perm * fvc::grad(h + z);
+        phi = fvc::flux(U);
+
+        //End bits
+        runTime.write();
+
+        Foam::Info << "ExecutionTime = " << runTime.elapsedCpuTime()   << " s"
+                   << "    ClockTime = " << runTime.elapsedClockTime() << " s"
+                   << nl << endl;
+    }
+
+    Foam::Info<< "End\n" << endl;
+    return 0;
+}
