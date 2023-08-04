4a4937cabfa981a7f88ffec4e186f3c88fe56e1f
Edwin <esaavedrac@u.northwestern.edu>
2023-08-04 16:13:59 -0500

    Reading boundary probes and plots

--- tests/optimizer_workflow/pyopenfoam.py
+++ tests/optimizer_workflow/pyopenfoam.py
@@ -5,14 +5,139 @@ from myusefultools.parser import getVTKList
 import pyvista as pv
 import numpy as np
 import xarray as xr
+import matplotlib.pyplot as plt
+from dataclasses import dataclass
+from fractions import Fraction
 
+
+@dataclass(slots=True, frozen=True)
+class probePoint:
+    x: float
+    y: float
+    z: float
+
+
+@dataclass
+class boundaryProbe:
+    path_data: str | Path
+    path_time: str | Path
+    path_xyz: str | Path
+
+    def __post_init__(self):
+        for path in [self.path_data, self.path_time, self.path_xyz]:
+            if isinstance(path, str):
+                path = Path(path)
+
+        ## Get the number of probed fields
+        self.fields_names = self.path_data.stem.replace("points_", "").split("_")
+        self.n_fields = len(self.fields_names)
+
+        ## Get the number of probes
+        self.get_probe_points()
+        self.n_probes = len(self.probes_points)
+
+        ## Get the timesteps
+        self.get_times()
+
+        ## Determine if vector or scalar data
+        self.set_dimensionality()
+
+        ## Initialize data arrays
+        if self.dimensionality == "scalar":
+            self.parse_data()
+
+    def infer_number_data_columns(self):
+        """Should be n_probes * n_fields * (1 or 3)"""
+        with open(self.path_data) as f:
+            first_line = f.readline().split()
+            self.n_cols = len(first_line)
+
+    def set_dimensionality(self):
+        self.infer_number_data_columns()
+
+        if self.n_cols == self.n_probes * self.n_fields:
+            self.dimensionality = "scalar"
+        elif self.n_cols == 3 * self.n_probes * self.n_fields:
+            self.dimensionality = "vector"
+        else:
+            print("This is not working")
+
+    def get_probe_points(self):
+        xyz = np.loadtxt(self.path_xyz)
+        self.probes_points = [probePoint(*coord) for coord in xyz]
+
+    def get_times(self):
+        self.list_of_times = np.loadtxt(self.path_time)
+
+    def parse_data(self):
+        full_data = np.loadtxt(self.path_data).T
+
+        data = dict()
+
+        for i, field in enumerate(self.fields_names):
+            # data[field] = full_data[i::self.n_fields]
+
+            data[field] = xr.DataArray(
+                full_data[i :: self.n_fields],
+                dims=("probe", "time"),
+                coords={"probe": self.probes_points, "time": self.list_of_times},
+            )
+
+        self.array_data = xr.Dataset(
+            data, coords={"time": self.list_of_times, "probes": self.probes_points}
+        )
+
+
+@dataclass
+class OperationSchedule:
+    dry_minutes: int
+    flood_minutes: int
+    end_minutes: int
+
+    def __post_init__(self):
+        self.current_time = 0.0
+        self.next_time_minutes = 0.0
+
+    @property
+    def flood_dry_ratio(self):
+        return Fraction(self.flood_minutes, self.dry_minutes)
+
+    def __bool__(self):
+        return self.current_time < self.end_minutes
+
+
+@dataclass
 class OpenFOAM:
-    def __init__(self, path: str) -> None:
-        self.path_case = Path(path)
-        self.path_vtk = self.path_case/"VTK"
+    path_case: str | Path
+    schedule: OperationSchedule
+
+    def __post_init__(self) -> None:
+        if isinstance(self.path_case, str):
+            self.path_case = Path(self.path_case)
+
+        self.path_vtk = self.path_case / "VTK"
+
+        if not self.path_case.exists():
+            self.spawn_from_template()
 
     def print_path_to_case(self):
         print(self.path_case)
+
+    def spawn_from_template(self):
+        subprocess.run(["mkdir", self.path_case])
+        subprocess.run(
+            [
+                "cp",
+                "-r",
+                "TEMPLATE/0.000",
+                "TEMPLATE/constant",
+                "TEMPLATE/system",
+                "TEMPLATE/VTK",
+                "TEMPLATE/VTK_soilProperties",
+                "TEMPLATE/organizedData",
+                self.path_case,
+            ]
+        )
 
     @property
     def latest_time(self) -> str:
@@ -31,14 +156,14 @@ class OpenFOAM:
             cwd=self.path_case,
             capture_output=True,
             text=True,
-            encoding="utf-8"
+            encoding="utf-8",
         )
 
         if foamListTimes.returncode == 0:
             if not foamListTimes.stdout:
                 return "0.000"
             else:
-                return foamListTimes.stdout.replace("\n","")
+                return foamListTimes.stdout.replace("\n", "")
 
         else:
             print("returnCode was not zero")
@@ -202,7 +327,7 @@ class OpenFOAM:
             cwd=self.path_case,
         )
 
-    def run_case(self, solver: Literal["unsatFoam", "unsatNutrientCycle"]):
+    def run_solver(self, solver: Literal["unsatFoam", "unsatNutrientCycle"]):
         """
         Executes the solver
 
@@ -215,11 +340,64 @@ class OpenFOAM:
         -------
         None
         """
-
         if solver not in ["unsatFoam", "unsatNutrientCycle"]:
             raise ValueError("Solver not allowed")
         else:
             subprocess.run([solver], stdout=subprocess.DEVNULL, cwd=self.path_case)
+
+    def run_case_by_schedule(self, verbose=False):
+        """
+        Executes the solver following the schedule instructions of dry
+        and flooding periods
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+
+        while self.schedule:
+            if verbose:
+                print("\nSIMULATION BEGINS")
+                print("\n☀️ \t Dry period", f"{self.schedule.current_time = }")
+                print(f"{self.latest_time = }")
+
+            self.schedule.next_time_minutes += self.schedule.dry_minutes
+
+            if verbose:
+                print("Run until", f"{self.schedule.next_time_minutes = }")
+
+            self.set_boundary_fixedGradient()  ## Dry
+            self.set_endtime(self.schedule.next_time_minutes)
+            self.run_solver("unsatFoam")
+
+            if verbose:
+                print("Dry period ended")
+
+            self.schedule.current_time = self.schedule.next_time_minutes
+
+            if not self.schedule:
+                break
+
+            if verbose:
+                print("\n🌧️ \t Flood period", f"{self.schedule.current_time = }")
+                print(f"{self.latest_time = }")
+
+            self.schedule.next_time_minutes += self.schedule.flood_minutes
+
+            if verbose:
+                print("Run until", f"{self.schedule.next_time_minutes = }")
+
+            self.set_boundary_fixedValue()  ##Flood
+            self.set_endtime(self.schedule.next_time_minutes)
+            self.run_solver("unsatFoam")
+
+            if verbose:
+                print("Flood period ended")
+            self.schedule.current_time = self.schedule.next_time_minutes
 
     def foam_to_vtk(self):
         """
@@ -233,9 +411,21 @@ class OpenFOAM:
         -------
         None
         """
+        path_soilParameters = Path("./constant/soilParameters/")
 
         subprocess.run("rm -r VTK VTK_soilProperties".split(), cwd=self.path_case)
-        subprocess.run("cp ./constant/soilParameters/* ./0.000/".split(), cwd=self.path_case)
+        subprocess.run(
+            [
+                "cp",
+                path_soilParameters / "alpha",
+                path_soilParameters / "K_0",
+                path_soilParameters / "n_vangenucthen",
+                path_soilParameters / "Sw_r",
+                path_soilParameters / "Sw_s",
+                "./0.000/",
+            ],
+            cwd=self.path_case,
+        )
 
         subprocess.run(
             [
@@ -246,12 +436,19 @@ class OpenFOAM:
                 '"0"',
             ],
             cwd=self.path_case,
-            stdout=subprocess.DEVNULL
+            stdout=subprocess.DEVNULL,
         )
 
         subprocess.run("mv VTK VTK_soilProperties".split(), cwd=self.path_case)
         subprocess.run(
-            ["rm", "0.000/alpha 0.000/K_0 0.000/n_vangenucthen 0.000/Sw_r 0.000/Sw_s"],
+            [
+                "rm",
+                "0.000/alpha",
+                "0.000/K_0",
+                "0.000/n_vangenucthen",
+                "0.000/Sw_r",
+                "0.000/Sw_s",
+            ],
             cwd=self.path_case,
         )
 
@@ -264,11 +461,58 @@ class OpenFOAM:
                 "-noZero",
             ],
             cwd=self.path_case,
-            stdout=subprocess.DEVNULL
+            stdout=subprocess.DEVNULL,
         )
         # foamToVTK -fields "(XAR XN XDN XI XARp XNp XDNp DOC NH4 NO3 O2 tracer BAP POCr Sw h porosity hydraulicCond U)"
-        
-    def read_field_all_times(self, field:str):
+
+    def boundaryProbes_to_txt(self):
+        """
+        Run pointFiles.sh on the case to parse the probe data into single files
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+            The following files are created:
+                - time.txt
+                - xyz.txt
+                - $field.xy
+        """
+
+        subprocess.call(
+            ["pointFiles.sh", "./postProcessing/boundaryProbes"],
+            cwd=self.path_case,
+        )
+
+    def process_boundaryProbes(self):
+        """
+        Read the processed boundaryProbes and parse them in xarrays
+
+        """
+        processed_probes_path = self.path_case / "organizedData/"
+        files = processed_probes_path.glob("points_*")
+        f_time = processed_probes_path / "time.txt"
+        f_xyz = processed_probes_path / "xyz.txt"
+
+        self.boundaryProbes = [boundaryProbe(f, f_time, f_xyz) for f in files]
+
+    def plot_xarray(self):
+        for bp in self.boundaryProbes:
+            if bp.dimensionality == "scalar":
+                for k, v in bp.array_data.items():
+                    fig, ax = plt.subplots()
+                    v.plot.line(x="time", ax=ax)
+
+                    plt.savefig(
+                        self.path_case
+                        / "organizedData/heatmaps"
+                        / f"{self.path_case.name}_xarray_{k}.png"
+                    )
+
+    def read_field_all_times(self, field: str):
         """
         Export all VTK timesteps to an xarray
 
@@ -282,36 +526,73 @@ class OpenFOAM:
         xr.Array
             With depth and time as the dimensions
         """
-    
+
         ## Extract VTK result (this should be done with a probe but meh)
-        all_vtk_paths = [self.path_vtk/f for f in getVTKList(self.path_vtk)]
+        all_vtk_paths = [self.path_vtk / f for f in getVTKList(self.path_vtk)]
         nTimes = len(all_vtk_paths)
-        times = [float(t) for t in subprocess.check_output("foamListTimes", cwd=self.path_case, text=True, encoding='utf-8').splitlines() if t[0:2].isnumeric()]
+        times = [
+            float(t)
+            for t in subprocess.check_output(
+                "foamListTimes", cwd=self.path_case, text=True, encoding="utf-8"
+            ).splitlines()
+            if t[0:2].isnumeric()
+        ]
 
         ## Use dimensions from the first VTK
         mesh = pv.read(all_vtk_paths[0])
-        line = pv.Line(
-            a:=[0, 0, mesh.bounds[5]],
-            b:=[0, 0, mesh.bounds[2]])
-        
-        sample = mesh.sample_over_line(a,b)
+        line = pv.Line(a := [0, 0, mesh.bounds[5]], b := [0, 0, mesh.bounds[2]])
+
+        sample = mesh.sample_over_line(a, b)
         nPoints = len(sample[field])
 
         ## Initialize array to store data
         results = np.zeros([nPoints, nTimes])
 
         ## Extract field for each vtk field
-        for t,vtk in enumerate(all_vtk_paths):
+        for t, vtk in enumerate(all_vtk_paths):
             mesh = pv.read(vtk)
-            sample = mesh.sample_over_line(a,b)
-            results[:,t] = sample[field]
+            sample = mesh.sample_over_line(a, b)
+            results[:, t] = sample[field]
 
         data = xr.DataArray(
-            results, 
-            dims=("z","t"), 
-            coords={
-                "z": sample.points[:,2], 
-                "t": times})
-    
+            results, dims=("z", "t"), coords={"z": sample.points[:, 2], "t": times}
+        )
+
         return data
-    
+
+    def plot_field_over_time(self, field: str, label: str, units: str):
+        """
+        Generates a Depth-time heatmap plot of the field
+
+        Parameters
+        ----------
+        field: str
+            Name of the field to plot.
+
+        Returns
+        -------
+        None
+            It should save a plot in the organizedData folder of the case
+
+        """
+
+        scalar = self.read_field_all_times(field)
+
+        igt = 0
+        fig, (cax, ax) = plt.subplots(
+            2, 1, figsize=[5, 7], gridspec_kw={"height_ratios": [0.2, 5]}, sharex=False
+        )
+        img = ax.pcolormesh(
+            scalar.t[igt:] / 86400, scalar.z, scalar[:, igt:], cmap="copper"
+        )
+        ax.spines.right.set_visible(False)
+        ax.set_xlabel("Time $t$ [d]")
+        ax.set_ylabel("Depth $z$ [m]")
+        plt.colorbar(img, cax=cax, orientation="horizontal")
+        cax.set_title(rf"${label}$ {units}")
+        fig.tight_layout()
+        plt.savefig(
+            self.path_case
+            / "organizedData/heatmaps"
+            / f"{self.path_case.name}_{field}.png"
+        )
--- tests/optimizer_workflow/run_cases.py
+++ tests/optimizer_workflow/run_cases.py
@@ -1,82 +1,132 @@
-from dataclasses import dataclass
-from fractions import Fraction
 from pathlib import Path
-from pyopenfoam import OpenFOAM
+from pyopenfoam import OpenFOAM, OperationSchedule
+import itertools
+import json
+import sys
+import multiprocessing as mp
+import argparse
 
 
-@dataclass
-class OperationSchedule:
-    flood_minutes: int
-    dry_minutes: int
-    end_minutes: int
+def read_list_cases_to_run(file: str | Path):
+    if isinstance(file, str):
+        file = Path(file)
 
-    def __post_init__(self):
-        self.current_time = 0.0
-        self.next_time_minutes = 0.0
-
-    @property
-    def flood_dry_ratio(self):
-        return Fraction(self.flood_minutes, self.dry_minutes)
-
-    def __bool__(self):
-        return self.current_time < self.end_minutes
+    with open(file) as f:
+        cases = json.load(f)
+    print(type(cases), cases)
+    return cases
 
 
-def main():
-    of = OpenFOAM("./TEMPLATE")
-    schedule = OperationSchedule(flood_minutes=20, dry_minutes=60, end_minutes=500)
+def main_postprocess():
+    cases = read_list_cases_to_run("list_cases_to_run.json")
 
-    print(f"{schedule = }")
+    for dry, flood in itertools.product(cases["dry_times"], cases["flood_times"]):
+        print(dry, flood)
+        of = OpenFOAM(f"./CASES/dry_{dry}__flood_{flood}")
+        of.foam_to_vtk()
+        of.plot_field_over_time(of.read_field_all_times("Sw"), label="Sw", units="-")
 
-    while schedule:
-    # while False:
 
-        print("\n☀️ \t Dry period", f"{schedule.current_time = }")
-        print(f"{of.latest_time = }")
-        
-        schedule.next_time_minutes += schedule.dry_minutes
-        print("Run until", f"{schedule.next_time_minutes = }")
+def main(args):
+    RUN_OPTIONS = args
+    json_file = RUN_OPTIONS["json"]
 
-        of.set_boundary_fixedGradient()  ## Dry
-        of.set_endtime(schedule.next_time_minutes)
-        of.run_case("unsatFoam")
+    ## Read cases
+    cases_json = read_list_cases_to_run(json_file)
+    cases_objs = dict()
 
-        print("Dry period ended")
-        schedule.current_time = schedule.next_time_minutes 
+    ## Assemble cases
+    for dry, flood in itertools.product(
+        cases_json["dry_times"], cases_json["flood_times"]
+    ):
+        schedule = OperationSchedule(
+            dry_minutes=dry, flood_minutes=flood, end_minutes=500
+        )
+        identifier = f"./CASES/dry_{dry}__flood_{flood}"
+        of = OpenFOAM(identifier, schedule=schedule)
+        cases_objs[identifier] = of
 
-        if not schedule:
-            break
+    print(cases_objs)
 
-        print("\n🌧️ \t Flood period", f"{schedule.current_time = }")
-        print(f"{of.latest_time = }")
+    if RUN_OPTIONS["run"]:
+        ## Pool for parallelizable tasks
+        with mp.Pool(processes=16) as pool:
+            pool.map(OpenFOAM.run_case_by_schedule, cases_objs.values())
 
-        schedule.next_time_minutes += schedule.flood_minutes
-        print("Run until", f"{schedule.next_time_minutes = }")
+    if RUN_OPTIONS["convert_vtk"]:
+        ## Export result to VTK
+        with mp.Pool() as pool:
+            pool.map(OpenFOAM.foam_to_vtk, cases_objs.values())
 
-        of.set_boundary_fixedValue()  ##Flood
-        of.set_endtime(schedule.next_time_minutes)
-        of.run_case("unsatFoam")
+    if RUN_OPTIONS["process_probes"]:
+        with mp.Pool() as pool:
+            pool.map(OpenFOAM.boundaryProbes_to_txt, cases_objs.values())
 
-        print("Flood period ended")
-        schedule.current_time = schedule.next_time_minutes
+    if RUN_OPTIONS["heatmap"]:
+        field = RUN_OPTIONS["heatmap"]
 
-        # print("*****************")
-    
-    of.foam_to_vtk()
+        with mp.Pool() as pool:
+            pool.starmap(
+                OpenFOAM.plot_field_over_time,
+                [(of, field, rf"${field}$", "[Units]") for of in cases_objs.values()],
+            )
 
-    scalar = of.read_field_all_times(field="Sw")
-    
-    igt = 0
-    import matplotlib.pyplot as plt
-    fig, (cax,ax) = plt.subplots(2,1, figsize=[5,7], gridspec_kw={"height_ratios":[0.2,5]}, sharex=False)
-    img = ax.pcolormesh(scalar.t[igt:]/86400, scalar.z, scalar[:,igt:], cmap="copper")
-    ax.spines.right.set_visible(False)
-    ax.set_xlabel("Time $t$ [d]")
-    ax.set_ylabel("Depth $z$ [m]")
-    plt.colorbar(img, cax=cax, orientation="horizontal")
-    # cax.set_title(fr"${label}$ {units}")
-    fig.tight_layout()
-    plt.savefig("field.png")
+    if RUN_OPTIONS["timeseries_probes"]:
+        # Initialize xarrays with the boundaryProbes
+        for of in cases_objs.values():
+            of.process_boundaryProbes()
+
+        # Generate plots for all the scalar probes and fields
+        # ..TODO: Expand for vector probes
+
+        with mp.Pool() as pool:
+            pool.map(OpenFOAM.plot_xarray, cases_objs.values())
+
+    # scalar = of.read_field_all_times(field="Sw")
+
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Handle multiple OpenFOAM unsatFoam cases alternating \
+            drying and flooding periods."
+    )
+    parser.add_argument(
+        "--json", action="store", help="json configuration file.", required=True
+    )
+
+    parser.add_argument(
+        "--run", action="store_true", help="Run the cases specified in the json file."
+    )
+
+    parser.add_argument(
+        "--convert_vtk",
+        action="store_true",
+        help="Convert results into VTK for postprocessing.",
+    )
+
+    parser.add_argument(
+        "--process_probes",
+        action="store_true",
+        help="Process the data from a probe into easily importable files.",
+    )
+
+    parser.add_argument(
+        "--heatmap",
+        action="store",
+        help="Make a heatmap plot of depth and time of the specified field.",
+    )
+
+    parser.add_argument(
+        "--timeseries_probes",
+        action="store_true",
+        help="Plot the xarray boundaryProbe",
+    )
+
+    print(vars(parser.parse_args()))
+
+    if len(sys.argv) <= 1:
+        print("Get help:", "\n  $ python run_cases.py --help")
+
+    else:
+        args = vars(parser.parse_args())
+        main(args)
